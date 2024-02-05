7163c7618db77dd491f94d8fb845e10d4648589c
akdemirsafak <akdemirsafak@gmail.com>
2024-02-05 11:39:42 +0300

    District Request, Response models added with validators. Mapper created for models. Service and repository classes created and Injected in Program.cs Base Response model created.

--- eniyisinerede.API/Controllers/DistrictController.cs
+++ eniyisinerede.API/Controllers/DistrictController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using eniyisinerede.API.RequestModels.District;
+using eniyisinerede.API.Service;
+using Microsoft.AspNetCore.Mvc;
 
 namespace eniyisinerede.API.Controllers;
 
@@ -6,33 +8,41 @@ namespace eniyisinerede.API.Controllers;
 [ApiController]
 public class DistrictController : ControllerBase
 {
+    private readonly IDistrictService _districtService;
+
+    public DistrictController(IDistrictService districtService)
+    {
+        _districtService = districtService;
+    }
+
     [HttpGet]
     public async Task<IActionResult> Get()
     {
-        return Ok();
+        return Ok(await _districtService.GetAllAsync());
     }
 
     [HttpGet("{id}")]
     public async Task<IActionResult> GetById(int id)
     {
-        return Ok();
+        return Ok(await _districtService.GetByIdAsync(id));
     }
 
     [HttpPost]
-    public async Task<IActionResult> Create()
+    public async Task<IActionResult> Create([FromBody] CreateDistrictRequest request)
     {
-        return Ok();
+        return Ok(await _districtService.CreateAsync(request));
     }
 
     [HttpPut("{id}")]
-    public async Task<IActionResult> Update()
+    public async Task<IActionResult> Update(int id, [FromBody] UpdateDistrictRequest request)
     {
-        return Ok();
+        return Ok(await _districtService.UpdateAsync(id,request));
     }
 
     [HttpDelete("{id}")]
     public async Task<IActionResult> Delete(int id)
     {
+        await _districtService.DeleteAsync(id);
         return Ok();
     }
 }
--- eniyisinerede.API/Models/District.cs
+++ eniyisinerede.API/Models/District.cs
@@ -3,6 +3,6 @@
 public class District : BaseModel
 {
     public string Name { get; set; }
-    public string ZipCode { get; set; }
+    public string? ZipCode { get; set; }
     public int CityId { get; set; }
 }
