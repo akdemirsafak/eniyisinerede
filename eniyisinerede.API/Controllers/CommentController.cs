53621595f582c61205ca2c3e45d1c4a10c88b584
akdemirsafak <akdemirsafak@gmail.com>
2024-02-05 14:30:32 +0300

    comment completed with validators

--- eniyisinerede.API/Controllers/CommentController.cs
+++ eniyisinerede.API/Controllers/CommentController.cs
@@ -1,39 +1,48 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using eniyisinerede.API.RequestModels.Comment;
+using eniyisinerede.API.Service;
+using Microsoft.AspNetCore.Mvc;
 
-namespace eniyisinerede.API.Controllers
+namespace eniyisinerede.API.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class CommentController : ControllerBase
 {
-    [Route("[controller]")]
-    [ApiController]
-    public class CommentController : ControllerBase
+    private readonly ICommentService _commentService;
+
+    public CommentController(ICommentService commentService)
     {
-        [HttpGet]
-        public async Task<IActionResult> Get()
-        {
-            return Ok();
-        }
+        _commentService = commentService;
+    }
 
-        [HttpGet("{id}")]
-        public async Task<IActionResult> GetById(int id)
-        {
-            return Ok();
-        }
+    [HttpGet]
+    public async Task<IActionResult> Get()
+    {
+        return Ok(await _commentService.GetAllAsync());
+    }
 
-        [HttpPost]
-        public async Task<IActionResult> Create()
-        {
-            return Ok();
-        }
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetById(int id)
+    {
+        return Ok(await _commentService.GetByIdAsync(id));
+    }
 
-        [HttpPut("{id}")]
-        public async Task<IActionResult> Update()
-        {
-            return Ok();
-        }
+    [HttpPost]
+    public async Task<IActionResult> Create([FromBody] CreateCommentRequest request)
+    {
+        return Ok(await _commentService.CreateAsync(request));
+    }
 
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> Delete(int id)
-        {
-            return Ok();
-        }
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Update(int id, [FromBody] UpdateCommentRequest request)
+    {
+        return Ok(await _commentService.UpdateAsync(id, request));
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Delete(int id)
+    {
+        await _commentService.DeleteAsync(id);
+        return Ok();
     }
 }
---
+++ eniyisinerede.API/Service/CommentService.cs
@@ -0,0 +1,57 @@
+﻿using eniyisinerede.API.Mappers.Comments;
+using eniyisinerede.API.Repository;
+using eniyisinerede.API.RequestModels.Comment;
+using eniyisinerede.API.ResponseModels.Comment;
+
+namespace eniyisinerede.API.Service
+{
+    public class CommentService : ICommentService
+    {
+        private readonly ICommentRepository _commentRepository;
+        CommentMapper _commentMapper = new CommentMapper();
+
+        public CommentService(ICommentRepository commentRepository)
+        {
+            _commentRepository = commentRepository;
+        }
+
+        public async Task<CreatedCommentResponse> CreateAsync(CreateCommentRequest commentRequest)
+        {
+            var comment= _commentMapper.CreateCommentRequestToComment(commentRequest);
+            var createdComment = await _commentRepository.CreateAsync(comment);
+            return _commentMapper.CommentToCreatedCommentResponse(createdComment);
+
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var result = await _commentRepository.DeleteAsync(id);
+            if (result == 0)
+            {
+                throw new Exception("Comment not found");
+            }
+        }
+
+        public async Task<List<CommentResponse>> GetAllAsync()
+        {
+            var comments= await _commentRepository.GetAllAsync();
+            return _commentMapper.CommentsToCommentListResponse(comments);
+        }
+
+        public async Task<CommentResponse> GetByIdAsync(int id)
+        {
+            var comment = await _commentRepository.GetByIdAsync(id);
+            if (comment is null)
+                throw new Exception("Comment not found");
+            return _commentMapper.CommentToCommentResponse(comment);
+        }
+
+        public async Task<UpdatedCommentResponse> UpdateAsync(int id, UpdateCommentRequest commentRequest)
+        {
+            var comment= _commentMapper.UpdateCommentRequestToComment(commentRequest);
+            comment.Id = id;
+            var updatedComment = await _commentRepository.UpdateAsync(comment);
+            return _commentMapper.CommentToUpdatedCommentResponse(updatedComment);
+        }
+    }
+}
