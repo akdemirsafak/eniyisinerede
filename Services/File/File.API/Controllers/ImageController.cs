c3bf4782a99f89dfb5ca65025c9b0a95be516acb
akdemirsafak <akdemirsafak@gmail.com>
2024-02-15 11:27:54 +0300

    Completed without test

---
+++ Services/File/File.API/Controllers/ImageController.cs
@@ -0,0 +1,21 @@
+﻿using File.API.Services;
+using Microsoft.AspNetCore.Mvc;
+using SharedLibrary.Controllers;
+
+namespace File.API.Controllers;
+
+public class ImageController(IFileService _fileService) : CustomBaseController
+{
+    [HttpPost]
+    public async Task<IActionResult> Upload(IFormFile image)
+    {
+
+        return CreateActionResult(await _fileService.UploadAsync(image, "images"));
+
+    }
+    [HttpDelete]
+    public async Task<IActionResult> Delete(string fileName)
+    {
+        return CreateActionResult(await _fileService.DeleteAsync(fileName, "images"));
+    }
+}
---
+++ Services/File/File.API/Models/BlobResponseDto.cs
@@ -0,0 +1,15 @@
+﻿namespace File.API.Models;
+
+public class BlobResponseDto
+{
+    public BlobResponseDto()
+    {
+    }
+    public BlobResponseDto(BlobDto blob)
+    {
+        Blob = blob;
+    }
+    public BlobDto Blob { get; set; }
+    public string? Message { get; set; }
+    public bool IsSuccess { get; set; }
+}
