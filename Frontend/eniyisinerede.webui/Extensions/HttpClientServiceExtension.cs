1f83415ba0e3f4e20915b26ab2e90d5272b25e09
akdemirsafak <akdemirsafak@gmail.com>
2024-02-16 13:41:09 +0300

    HttpClientServiceExtension added and httpclient settings moved.

---
+++ Frontend/eniyisinerede.webui/Extensions/HttpClientServiceExtension.cs
@@ -0,0 +1,48 @@
+﻿using eniyisinerede.webui.Services;
+using eniyisinerede.webui.Services.Interfaces;
+
+namespace eniyisinerede.webui.Extensions;
+
+public static class HttpClientServiceExtension
+{
+    public static void AddHttpClientServices(this IServiceCollection serviceCollection)
+    {
+        serviceCollection.AddHttpClient<IPlaceService, PlaceService>(options =>
+        {
+            options.BaseAddress = new Uri("https://localhost:5010/api/");
+        });
+
+        serviceCollection.AddHttpClient<IProductService, ProductService>(options =>
+        {
+            options.BaseAddress = new Uri("https://localhost:5012/api/");
+        });
+
+        serviceCollection.AddHttpClient<IReservationService, ReservationService>(options =>
+        {
+            options.BaseAddress = new Uri("https://localhost:5013/api/");
+        });
+
+        serviceCollection.AddHttpClient<ICountryService, CountryService>(options =>
+        {
+            options.BaseAddress = new Uri("https://localhost:5011/api/");
+        });
+
+        serviceCollection.AddHttpClient<ICityService, CityService>(options =>
+        {
+            options.BaseAddress = new Uri("https://localhost:5011/api/");
+        });
+
+        serviceCollection.AddHttpClient<IDistrictService, DistrictService>(options =>
+        {
+            options.BaseAddress = new Uri("https://localhost:5011/api/");
+        });
+        serviceCollection.AddHttpClient<IFileService, FileService>(options =>
+        {
+            options.BaseAddress = new Uri("https://localhost:5014/api/");
+        });
+        serviceCollection.AddHttpClient<IImageService, ImageService>(options =>
+        {
+            options.BaseAddress = new Uri("https://localhost:5014/api/");
+        });
+    }
+}
--- Frontend/eniyisinerede.webui/Program.cs
+++ Frontend/eniyisinerede.webui/Program.cs
@@ -1,5 +1,5 @@
+using eniyisinerede.webui.Extensions;
 using eniyisinerede.webui.Services;
-using eniyisinerede.webui.Services.Interfaces;
 using Microsoft.Extensions.DependencyModel;
 using Scrutor;
 using System.Reflection;
@@ -25,44 +25,7 @@ builder.Services.Scan(scan => scan.FromAssemblies(assemblies)
 
 builder.Services.AddAutoMapper(typeof(Program));
 
-
-builder.Services.AddHttpClient<IPlaceService, PlaceService>(options =>
-{
-    options.BaseAddress = new Uri("https://localhost:5010/api/");
-});
-
-builder.Services.AddHttpClient<IProductService, ProductService>(options =>
-{
-    options.BaseAddress = new Uri("https://localhost:5012/api/");
-});
-
-builder.Services.AddHttpClient<IReservationService, ReservationService>(options =>
-{
-    options.BaseAddress = new Uri("https://localhost:5013/api/");
-});
-
-builder.Services.AddHttpClient<ICountryService, CountryService>(options =>
-{
-    options.BaseAddress = new Uri("https://localhost:5011/api/");
-});
-
-builder.Services.AddHttpClient<ICityService, CityService>(options =>
-{
-    options.BaseAddress = new Uri("https://localhost:5011/api/");
-});
-
-builder.Services.AddHttpClient<IDistrictService, DistrictService>(options =>
-{
-    options.BaseAddress = new Uri("https://localhost:5011/api/");
-});
-builder.Services.AddHttpClient<IFileService, FileService>(options =>
-{
-    options.BaseAddress = new Uri("https://localhost:5014/api/");
-});
-builder.Services.AddHttpClient<IImageService, ImageService>(options =>
-{
-    options.BaseAddress = new Uri("https://localhost:5014/api/");
-});
+builder.Services.AddHttpClientServices();
 
 
 
